f2de47b4c6f454f60ec19772fe3c4e4ca5a865d0
Silvia Muthangya <silvia.muthangya@Softwaregroup-bg.com>
2023-01-23 18:17:01 +0300

    [function]: returns pascal's triangle to the nth number

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,24 @@
+"""
+    a function def pascal_triangle(n): that returns a list of lists of integers representing the Pascal’s triangle of n
+
+        Returns an empty list if n <= 0
+        assumes n will be always an integer
+
+"""
+from math import factorial
+
+def pascal_triangle(n):
+    list_lists = []
+    small_list = []
+    if n <= 0:
+        return []
+    else:
+        for i in range(n):
+            for j in range(i+1):
+                # nCr = n!/((n-r)!*r!)
+                small_list.append(factorial(i)//(factorial(j)*factorial(i-j)))
+
+            list_lists.append(small_list)
+            small_list = []
+
+        return list_lists
