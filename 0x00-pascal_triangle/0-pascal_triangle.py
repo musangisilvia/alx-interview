6ba44849c1cd66229910c79c8f99c707400cb40e
Silvia Muthangya <silvia.muthangya@Softwaregroup-bg.com>
2023-01-23 18:35:00 +0300

    pep8 validation

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python
 """
-    a function def pascal_triangle(n): that returns a list of lists of integers representing the Pascal’s triangle of n
+    a function def pascal_triangle(n): that returns a list of lists
+    of integers representing the Pascal’s triangle of n
 
         Returns an empty list if n <= 0
         assumes n will be always an integer
 
 """
 from math import factorial
+
 
 def pascal_triangle(n):
     """
