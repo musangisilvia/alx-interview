7211d9891613f60f7e8bb87ac4a00c12409004a9
Silvia Muthangya <musangisilvia@gmail.com>
2023-04-19 16:12:38 +0300

    add solution

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""
+
+    Module: 0-make_change
+
+"""
+
+
+def makeChange(coins, total):
+    """
+        Function that makes change of the total from the coins
+        Params: coins
+                total
+        Return: fewest number of coins needed to meet total
+                If total is 0 or less, return 0
+                If total cannot be met by any number of coins you have,
+                return -1
+    """
+    # least coins
+    coin_count = 0
+    if total > 0:
+        while total > 0 and coins:
+            greatest = max(coins)
+            if greatest < total:
+                while greatest < total:
+                    total -= greatest
+                    coin_count += 1
+
+                coins.remove(greatest)
+            else:
+                return -1
+
+        return coin_count
+
+    return 0
